ae0fa207a05246e9b3ba0f7e4c8e5a5618a3bcce
Eason Liu <yql@yorku.ca>
2024-04-03 14:43:31 -0400

    Fix ResBlock

--- src/models.py
+++ src/models.py
@@ -516,22 +516,22 @@ class TokenNoising(nn.Module):
 
 
 class ResBlock(nn.Module):
-    def __init__(self, in_channels, out_channels):
+    def __init__(self, in_channels, res_channels):
         super(ResBlock, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1)
-        self.relu = nn.ReLU(inplace=True)
-        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=1)
-        self.relu = nn.ReLU(inplace=True)
+        self.conv1 = nn.Conv2d(in_channels, res_channels, kernel_size=3, padding=1)
+        self.relu1 = nn.ReLU()
+        self.conv2 = nn.Conv2d(res_channels, in_channels, kernel_size=1)
+        self.relu2 = nn.ReLU()
 
     def forward(self, x):
         residual = x
+
         out = self.conv1(x)
-        out = self.relu(out)
-
+        out = self.relu1(out)
         out = self.conv2(out)
-        out = self.relu(out)
+        out = self.relu2(out)
 
-        out += residual
+        out = out + residual
         out = F.relu(out)
         return out
 
