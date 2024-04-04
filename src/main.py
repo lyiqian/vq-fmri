1e0a08835e11fcf4e8121badc5e5eeb941a80e69
Eason Liu <yql@yorku.ca>
2024-04-04 14:59:34 -0400

    Include super res in decode_frmi function

--- src/main.py
+++ src/main.py
@@ -1,24 +1,33 @@
 import models
 
 def main():
+    """Inference the first 10 fMRI with trained networks"""
+    # load test fMRI
+
+    # load networks
+
+    # inference
     pass
 
 
-def inference(
+def decode_fmri(
     fmri,
-    vq_vae: models.VqVaeAbc,
+    vq_l: models.VectorQuantizerAbc,
     fmri_enc: models.FMRIEncoderAbc,
     token_clf: models.TokenClassifierAbc,
     inpainting: models.InpaintingNetworkAbc,
+    sr: models.SuperResolutionAbc,
+    img_dec: models.ImageDecoderAbc,
 ):
     spatial_feats = fmri_enc.encode(fmri)
 
-    spatial_token = vq_vae.quantize(spatial_feats)
-    noise_table = token_clf.predict(spatial_token)
+    spatial_token_l = vq_l.quantize(spatial_feats)
+    noise_table = token_clf.predict(spatial_token_l)
 
-    visual_cues = inpainting(spatial_token, noise_table)
+    visual_cues_l = inpainting(spatial_token_l, noise_table)
+    visual_cues = sr.transform(visual_cues_l)
 
-    reconstructed_img = vq_vae.decode(visual_cues)
+    reconstructed_img = img_dec.decode(visual_cues)
     return reconstructed_img
 
 
